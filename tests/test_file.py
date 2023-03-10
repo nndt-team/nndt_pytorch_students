585e820c6a3774ee89580f10ece81bac885b0d87
Maksim Dzhigil <72609950+maksim-dzhigil@users.noreply.github.com>
2023-03-10 12:44:49 +0500

    broke test to check jenkins

--- tests/test_file.py
+++ tests/test_file.py
@@ -3,7 +3,11 @@ import math
 
 def test_method():
     assert math.log(100, 10) == 2, "Should be 2"
+    
+def test_broken_method():
+    assert 2 == 3
 
 
 if __name__ == "__main__":
     test_method()
+    test_broken_method()
